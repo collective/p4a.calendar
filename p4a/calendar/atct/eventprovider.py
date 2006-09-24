6764b46d5d857f86acf995cfa0975c2e3d8b9fe7
rocky <rocky@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2006-09-24 22:08:48 +0000

    Can now activate calendar monthly view for a smart folder.

--- p4a/calendar/atct/eventprovider.py
+++ p4a/calendar/atct/eventprovider.py
@@ -5,6 +5,7 @@ from p4a.calendar import interfaces
 from DateTime import DateTime
 from Products.CMFCore import utils as cmfutils
 from Products.Archetypes import atapi
+from Products.ATContentTypes.content import topic
 
 def dt2DT(dt):
     s = "%04i-%02i-%02i %02i:%02i" % (dt.year, dt.month, dt.day, dt.hour, dt.minute)
@@ -26,3 +27,13 @@ class ATEventProvider(object):
                                end={'query': dt2DT(stop), 'range': 'max'})
 
         return event_brains
+
+class TopicEventProvider(object):
+    interface.implements(interfaces.IEventProvider)
+    component.adapts(topic.ATTopic)
+
+    def __init__(self, context):
+        self.context = context
+        
+    def gather_events(self, start, stop):
+        return self.context.queryCatalog()
