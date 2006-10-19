9db1face7f30ff77625564fbdf97b5c9bcd4c996
lennartregebro <lennartregebro@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2006-10-19 21:06:56 +0000

    New less calendary and more news-y event lists.

---
+++ p4a/calendar/browser/events.py
@@ -0,0 +1,58 @@
+import datetime
+import calendar
+try:
+    from Products.Five.browser.pagetemplatefile import \
+         ZopeTwoPageTemplateFile as PageTemplateFile
+except ImportError:
+    from zope.pagetemplate.pagetemplatefile import PageTemplateFile
+
+from p4a.calendar import interfaces
+
+class EventListingView(object):
+    """View that lists events.
+    """
+    
+    eventlist = PageTemplateFile('events.pt')
+    
+    def _getEventList(self, start=None, stop=None):
+        provider = interfaces.IEventProvider(self.context)
+        now = datetime.datetime.now()
+        events = list(provider.gather_events(start=start, stop=stop))
+        events.sort()
+        months = []
+        month_info = []
+        old_month_year = None
+        for event in events:
+            start = event.start
+            month = str(start.month)
+            year = str(start.year)
+            month_year = year+month
+            if month_year != old_month_year:
+                if month_info:
+                    months.append(month_info)
+                month_info = {'month': start.month,
+                              'year': start.year,
+                              'month_name': start.strftime("%B"),
+                              'events': []}
+            event_dict = {'event': event,
+                          'day': start.day,
+                          'title': event.title,
+                          'description': event.description,
+                          'url': event.local_url,
+                          }
+            month_info['events'].append(event_dict)
+
+        if month_info:
+            months.append(month_info)
+            
+        return months
+        
+    def upcomingEvents(self):
+        now = datetime.datetime.now()
+        months = self._getEventList(start=now)
+        return self.eventlist(months=months)
+
+    def pastEvents(self):
+        now = datetime.datetime.now()
+        months = self._getEventList(stop=now)
+        return self.eventlist(months=months)
