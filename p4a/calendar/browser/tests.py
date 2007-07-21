7dc71095029b5ecf8028bfa525fc7fa133cf959d
aaronvanderlip <aaronvanderlip@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-07-21 18:42:19 +0000

    =reverted to proper working tests

--- p4a/calendar/browser/tests.py
+++ p4a/calendar/browser/tests.py
@@ -1,13 +1,10 @@
 import unittest
 from zope.testing import doctest
-from p4a.calendar.browser.test_month import TestHourTimeFormatter
 
 def test_suite():
-    suite = unittest.TestSuite((
+    return unittest.TestSuite((
         doctest.DocTestSuite('p4a.calendar.browser.month'),
         ))
-    suite.addTest(unittest.makeSuite(TestHourTimeFormatter))
-    return suite
 
 if __name__ == "__main__":
     unittest.main(defaultTest='test_suite')
